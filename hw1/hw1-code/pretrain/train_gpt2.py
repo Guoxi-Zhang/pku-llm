732a1574175291ad885303a8590df2d6f46aa3bf
Guoxi Zhang <zhangguoxi64@gmail.com>
2024-11-06 00:54:06 +0800

    [2401210151] feat: 学习率动态设置，权重衰退，fused AdamW

--- hw1/hw1-code/pretrain/train_gpt2.py
+++ hw1/hw1-code/pretrain/train_gpt2.py
@@ -1,5 +1,6 @@
 
 
+import math
 import time
 import torch
 from torch.nn import functional as F
@@ -39,8 +40,26 @@ train_loader = DataLoaderLite(B=8, T=1024)
 torch.set_float32_matmul_precision('high')
 
 # ------------------------------  优化器  ------------------------------
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8) # type: ignore
-for i in range(50):
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+def get_lr(it: int) -> float:
+    # 1) 线性warmup
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) lr > max_steps时，返回min_lr
+    if it > max_steps:
+        return min_lr
+    # 3) 介于两者之间时，使用余弦衰减到min_lr
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+# ------------------------------  训练模型  ------------------------------
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -51,14 +70,17 @@ for i in range(50):
         # import code; code.interact(local=locals())
     loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # 确定本次迭代的学习率
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     torch.cuda.synchronize() # wait for the GPU to finish work
     t1 = time.time()
     dt = t1 - t0 # time difference in seconds
     tokens_processed = train_loader.B * train_loader.T
     tokens_per_sec = tokens_processed / dt
-    print(f"step {i:4d} | loss: {loss.item():.6f} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
-
+    print(f"step {step:4d} | loss: {loss.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 import sys; sys.exit(0)
 
 # ------------------------------  评估模型生成文本  ------------------------------
